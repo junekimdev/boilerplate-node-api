f38a95b2498d7501a0efe4527e6a96d80ce7cecd
June Kim <godlyjune@gmail.com>
2023-05-24 17:58:11 +0900

    Exclude index.ts files from testing

--- jest.config.js
+++ jest.config.js
@@ -17,7 +17,7 @@ module.exports = {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ['**/*.ts', '!**/node_modules/**'],
+  collectCoverageFrom: ['**/*.ts', '!**/index.ts', '!**/node_modules/**'],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'test/coverage',
