fe7aab93b6aae22922e532caeb9d98e130daf4c2
June Kim <godlyjune@gmail.com>
2023-05-26 19:06:51 +0900

    Add unit tests for functions in pinoExpOpt

--- src/test/unit/logger.test.ts
+++ src/test/unit/logger.test.ts
@@ -1,4 +1,3 @@
-import pino from 'pino';
 import { logger, pinoExpOpt } from '../../utils/logger';
 
 describe('Test /src/util/logger', () => {
@@ -9,11 +8,38 @@ describe('Test /src/util/logger', () => {
   });
 
   describe('pinoExpOpt', () => {
-    it('should have methods: [customReceivedMessage, customSuccessMessage]', () => {
+    it('should have a logger', () => {
+      expect(pinoExpOpt.logger).toEqual(logger);
+    });
+
+    it('should implment methods: [customReceivedMessage, customSuccessMessage]', () => {
       expect(pinoExpOpt).toMatchObject({
         customReceivedMessage: expect.any(Function),
         customSuccessMessage: expect.any(Function),
       });
+    });
+  });
+
+  describe('pinoExpOpt.customReceivedMessage()', () => {
+    it('should return a string', () => {
+      const req = jest.fn(() => ({ method: 'method' }));
+      const res = jest.fn(() => ({ url: 'test_url' }));
+
+      //@ts-ignore
+      const str = pinoExpOpt.customReceivedMessage(req, res);
+
+      expect(str).toEqual(expect.any(String));
+    });
+  });
+
+  describe('pinoExpOpt.customSuccessMessage()', () => {
+    it('should return a string', () => {
+      const res = jest.fn(() => ({ statusCode: 200 }));
+
+      //@ts-ignore
+      const str = pinoExpOpt.customSuccessMessage(res);
+
+      expect(str).toEqual(expect.any(String));
     });
   });
 });
