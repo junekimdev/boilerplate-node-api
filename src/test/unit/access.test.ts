ea6f746444bef6d8d8e8d2ac21c05d6a1eceab4a
June Kim <godlyjune@gmail.com>
2023-06-01 02:44:37 +0900

    Upgrade access util

--- src/test/unit/access.test.ts
+++ src/test/unit/access.test.ts
@@ -1,9 +1,4 @@
-import {
-  AccessControlResultRow,
-  convertToString,
-  isReadable,
-  isWritable,
-} from '../../utils/access';
+import { AccessControlResultRow, convertToString } from '../../utils/access';
 
 describe('Test /src/utils/access', () => {
   describe('convertToString()', () => {
@@ -16,17 +11,6 @@ describe('Test /src/utils/access', () => {
 
       const result = convertToString(row);
       expect(result).toBeUndefined();
-    });
-
-    it('should return the resource name if row is readable and writable', () => {
-      const row: AccessControlResultRow = {
-        name: 'resource',
-        readable: true,
-        writable: true,
-      };
-
-      const result = convertToString(row);
-      expect(result).toBe('resource');
     });
 
     it('should return the resource name followed by ":read" if row is readable and not writable', () => {
@@ -51,37 +35,16 @@ describe('Test /src/utils/access', () => {
 
       expect(result).toBe('resource:write');
     });
-  });
 
-  describe('isReadable()', () => {
-    const accessString = 'resource1 resource2:read resource3:write';
+    it('should return the resource name followed by ":read" and ":write" if row is readable and writable', () => {
+      const row: AccessControlResultRow = {
+        name: 'resource',
+        readable: true,
+        writable: true,
+      };
 
-    it('should return true if the resource is readable in the accessString', () => {
-      const result1 = isReadable(accessString, 'resource1');
-      const result2 = isReadable(accessString, 'resource2');
-      expect(result1).toBe(true);
-      expect(result2).toBe(true);
-    });
-
-    it('should return false if the resource is not readable in the accessString', () => {
-      const result = isReadable(accessString, 'resource3');
-      expect(result).toBe(false);
-    });
-  });
-
-  describe('isWritable()', () => {
-    const accessString = 'resource1 resource2:read resource3:write';
-
-    it('should return true if the resource is writable in the accessString', () => {
-      const result1 = isWritable(accessString, 'resource1');
-      const result2 = isWritable(accessString, 'resource3');
-      expect(result1).toBe(true);
-      expect(result2).toBe(true);
-    });
-
-    it('should return false if the resource is not writable in the accessString', () => {
-      const result = isWritable(accessString, 'resource2');
-      expect(result).toBe(false);
+      const result = convertToString(row);
+      expect(result).toBe('resource:read resource:write');
     });
   });
 });
--- src/utils/access.ts
+++ src/utils/access.ts
@@ -9,28 +9,10 @@ export interface AccessControlResultRow extends QueryResultRow {
 export const convertToString = (row: AccessControlResultRow) => {
   if (!row.readable && !row.writable) return;
 
-  let str = row.name;
-  if (row.readable && !row.writable) str += ':read'; // read-only
-  if (!row.readable && row.writable) str += ':write'; // write-only
-  return str;
+  let access = [];
+  if (row.readable) access.push(`${row.name}:read`);
+  if (row.writable) access.push(`${row.name}:write`);
+  return access.join(' ');
 };
 
-export const isReadable = (accessString: string, resource: string) => {
-  const accesses = accessString.split(' ');
-  for (let i = 0; i < accesses.length; i++) {
-    const access = accesses[i];
-    if (access.startsWith(resource) && !access.endsWith(':write')) return true;
-  }
-  return false;
-};
-
-export const isWritable = (accessString: string, resource: string) => {
-  const accesses = accessString.split(' ');
-  for (let i = 0; i < accesses.length; i++) {
-    const access = accesses[i];
-    if (access.startsWith(resource) && !access.endsWith(':read')) return true;
-  }
-  return false;
-};
-
-export default { convertToString, isReadable, isWritable };
+export default { convertToString };
