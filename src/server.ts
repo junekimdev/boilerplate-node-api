dc4afb11ad838a99f9b4db337bd34d9e4305e403
June Kim <godlyjune@gmail.com>
2023-06-03 00:04:10 +0900

    Remove error handler b/c commit 9d4c933

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,11 @@ import express, { NextFunction, Request, Response } from 'express';
 import pinoExpress from 'express-pino-logger';
 import helmet from 'helmet';
 import apiRouter from './api';
-import { corsOption, logger, pinoExpOpt } from './utils';
+import errorHandler from './errorHandler';
+import { corsOption } from './utils/cors';
+import { logger, pinoExpOpt } from './utils/logger';
 
-const { TRUST_PROXY = '', PORT = '3000', SERVICE_NAME = 'my_api' } = process.env;
+const { TRUST_PROXY = '', PORT = '3000' } = process.env;
 const app = express();
 
 // Tell Express if this app is behind a proxy
@@ -21,32 +23,8 @@ app.use(cors(corsOption)); // Enable CORS
 app.use(pinoExpress(pinoExpOpt)); // HTTP request logger middleware
 app.use('/api', apiRouter); // Routing middleware
 app.use(express.static('public')); // static files
-
-// Error Handler
-app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-  const errPresenter: any = {};
-  errPresenter.status = err.status || 500;
-  errPresenter.code = err.code || 'ERROR';
-  errPresenter.message = err.message;
-  if (req.app.get('env') === 'development') errPresenter.error = err;
-
-  // 401 MUST include a WWW-Authenticate header field
-  // @see https://tools.ietf.org/html/rfc7235#section-4.1
-  if (err.status === 401) {
-    const authType = err.code === 'E01' ? 'Basic' : 'Bearer';
-    res.set('WWW-Authenticate', `${authType} realm="${SERVICE_NAME}"`);
-  }
-
-  // No Error Loggings for AppErrors
-  if (!err.status) {
-    logger.error(`[${req.method} ${req.originalUrl}] from ${req.ip} | ${JSON.stringify(err)}`);
-  }
-
-  // render the error page
-  res.status(errPresenter.status).json(errPresenter);
-  next();
-});
+app.use(errorHandler); // Error Handler
 
 app.listen(PORT, () => logger.info(`Server started on port ${PORT}`));
 
-module.exports = app;
+export default app;
