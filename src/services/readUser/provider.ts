fbc6c8e3e0959a09a0ee0e14fd1567ef7138405c
June Kim <godlyjune@gmail.com>
2023-06-12 13:49:16 +0900

    Refactor SQL to prevent confusion

--- src/services/readUser/provider.ts
+++ src/services/readUser/provider.ts
@@ -1,9 +1,9 @@
 import db from '../../utils/db';
 
 const SQL_GET_USER = `SELECT
-UP.id, email, surname, given_name, UR.name as role_name, last_login, UP.created_at
-FROM userpool as UP LEFT JOIN user_role as UR ON UP.role_id = UR.id
-WHERE UP.id=$1::INT;`;
+T1.id, T1.email, T1.surname, T1.given_name, T2.name as role_name, T1.last_login, T1.created_at
+FROM userpool as T1 LEFT JOIN user_role as T2 ON T1.role_id=T2.id
+WHERE T1.id=$1::INT;`;
 
 const provider = async (userId: number) => {
   const result = await db.query(SQL_GET_USER, [userId]);
