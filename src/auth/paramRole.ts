3446392e1fe830e387891fa06138f2695f274343
June Kim <godlyjune@gmail.com>
2023-06-12 23:35:18 +0900

    Change name and location to pass data

--- src/auth/paramRole.ts
+++ src/auth/paramRole.ts
@@ -6,11 +6,14 @@ const SQL_GET_ROLES = 'SELECT id FROM user_role WHERE name=$1::VARCHAR(50);';
 
 const param = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const { role } = req.params;
+    const { role_name } = req.body;
+    if (typeof role_name !== 'string') throw new AppError(errDef[400].invalidRoleName);
 
     // Check if role is valid
-    const roleTable = await db.query(SQL_GET_ROLES, [role]);
+    const roleTable = await db.query(SQL_GET_ROLES, [role_name]);
     if (!roleTable.rowCount) throw new AppError(errDef[400].invalidRoleName);
+
+    res.locals.roleName = role_name;
     next();
   } catch (error) {
     next(error);
--- test/unit/auth/paramRole.test.ts
+++ test/unit/auth/paramRole.test.ts
@@ -15,16 +15,27 @@ describe('Test /src/auth/paramRole', () => {
   let res: Response;
   let next: NextFunction;
 
-  const role = 'test-role';
+  const role_name = 'test-role_name';
 
   beforeEach(() => {
-    req = { params: { role } } as unknown as Request;
+    req = { body: { role_name } } as unknown as Request;
     res = { locals: {} } as unknown as Response;
     next = jest.fn();
     jest.clearAllMocks();
   });
 
-  it('should call next with invalidRoleName error when role in params is not in DB', async () => {
+  it('should call next with invalidRoleName error when role_name in req.body is not a string', async () => {
+    const expectedError = new AppError(errDef[400].invalidRoleName);
+
+    req.body = { role_name: 123 };
+
+    await paramRole(req, res, next);
+
+    expect(db.query).not.toBeCalled();
+    expect(next).toBeCalledWith(expectedError);
+  });
+
+  it('should call next with invalidRoleName error when role_name in req.body is not in DB', async () => {
     const expectedError = new AppError(errDef[400].invalidRoleName);
 
     mockedDbQuery.mockResolvedValue({ rowCount: 0 });
@@ -32,17 +43,18 @@ describe('Test /src/auth/paramRole', () => {
     await paramRole(req, res, next);
 
     expect(db.query).toBeCalledTimes(1);
-    expect(db.query).toBeCalledWith(expect.any(String), [role]);
+    expect(db.query).toBeCalledWith(expect.any(String), [role_name]);
     expect(next).toBeCalledWith(expectedError);
   });
 
-  it('should call next when role in params found in DB', async () => {
+  it('should put roleName in res.locals and call next when role_name in req.body found in DB', async () => {
     mockedDbQuery.mockResolvedValue({ rowCount: 1 });
 
     await paramRole(req, res, next);
 
     expect(db.query).toBeCalledTimes(1);
-    expect(db.query).toBeCalledWith(expect.any(String), [role]);
+    expect(db.query).toBeCalledWith(expect.any(String), [role_name]);
+    expect(res.locals).toHaveProperty('roleName');
     expect(next).toBeCalledWith();
   });
 });
