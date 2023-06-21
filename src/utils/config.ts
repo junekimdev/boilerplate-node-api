9c4a1fa5bc99fd3ac78462209ea369891b3e4093
June Kim <godlyjune@gmail.com>
2023-06-21 17:26:35 +0900

    Add getProjectRoot()

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,13 +1,15 @@
 import dotenv from 'dotenv';
 import path from 'path';
 
+export const getProjectRoot = () => path.resolve(__dirname, '../../');
+
 export const config = () => {
   const { NODE_ENV } = process.env;
 
   if (NODE_ENV === 'test') {
     const { TEST_NAME } = process.env;
 
-    dotenv.config({ path: path.resolve(__dirname, '../../test/test.config') });
+    dotenv.config({ path: path.join(getProjectRoot(), '/test/test.config') });
     process.env.PGUSER = TEST_NAME;
     process.env.PGPASSWORD = TEST_NAME;
     process.env.PGDATABASE = TEST_NAME;
--- test/unit/utils/config.test.ts
+++ test/unit/utils/config.test.ts
@@ -1,11 +1,12 @@
 jest.mock('dotenv', () => ({ config: jest.fn() }));
-jest.mock('path', () => ({ resolve: jest.fn() }));
+jest.mock('path', () => ({ resolve: jest.fn(), join: jest.fn() }));
 
 import dotenv from 'dotenv';
 import path from 'path';
 
 const mockedDotenvConf = dotenv.config as jest.Mock;
 const mockedPathResolve = path.resolve as jest.Mock;
+const mockedPathJoin = path.join as jest.Mock;
 
 describe('Test /src/utils/config', () => {
   const testName = 'testName';
@@ -14,43 +15,58 @@ describe('Test /src/utils/config', () => {
     jest.clearAllMocks();
   });
 
-  it('should set environmental variables, PGUSER, PGPASSWORD, and PGDATABASE in test mode', () => {
-    const { NODE_ENV } = process.env;
-    process.env.TEST_NAME = testName;
-    const module = require('../../../src/utils/config');
-    const testPath = 'testPath';
+  describe('getProjectRoot()', () => {
+    it('should return a string of dir of project root', () => {
+      mockedPathResolve.mockRestore();
+      const root = path.resolve(__dirname, '../../../');
+      const module = require('../../../src/utils/config');
 
-    mockedPathResolve.mockReturnValue(testPath);
+      const result = module.getProjectRoot();
 
-    module.config();
-
-    const { PGUSER, PGPASSWORD, PGDATABASE } = process.env;
-
-    expect(NODE_ENV).toBe('test');
-    expect(path.resolve).toBeCalled();
-    expect(dotenv.config).toBeCalledWith({ path: testPath });
-    expect(PGUSER).toBe(testName);
-    expect(PGPASSWORD).toBe(testName);
-    expect(PGDATABASE).toBe(testName);
+      expect(result).toBe(root);
+    });
   });
 
-  it('should set environmental variables in development mode', () => {
-    process.env.NODE_ENV = 'development';
-    const module = require('../../../src/utils/config');
+  describe('config()', () => {
+    it('should set environmental variables, PGUSER, PGPASSWORD, and PGDATABASE in test mode', () => {
+      const { NODE_ENV } = process.env;
+      process.env.TEST_NAME = testName;
+      const module = require('../../../src/utils/config');
+      const testPath = 'testPath';
 
-    module.config();
+      mockedPathJoin.mockReturnValue(testPath);
 
-    expect(dotenv.config).toBeCalled();
-    expect(path.resolve).not.toBeCalled();
-  });
+      module.config();
 
-  it('should set environmental variables in production mode', () => {
-    process.env.NODE_ENV = 'production';
-    const module = require('../../../src/utils/config');
+      const { PGUSER, PGPASSWORD, PGDATABASE } = process.env;
 
-    module.config();
+      expect(NODE_ENV).toBe('test');
+      expect(path.resolve).toBeCalled();
+      expect(path.join).toBeCalled();
+      expect(dotenv.config).toBeCalledWith({ path: testPath });
+      expect(PGUSER).toBe(testName);
+      expect(PGPASSWORD).toBe(testName);
+      expect(PGDATABASE).toBe(testName);
+    });
 
-    expect(dotenv.config).toBeCalled();
-    expect(path.resolve).not.toBeCalled();
+    it('should set environmental variables in development mode', () => {
+      process.env.NODE_ENV = 'development';
+      const module = require('../../../src/utils/config');
+
+      module.config();
+
+      expect(dotenv.config).toBeCalled();
+      expect(path.join).not.toBeCalled();
+    });
+
+    it('should set environmental variables in production mode', () => {
+      process.env.NODE_ENV = 'production';
+      const module = require('../../../src/utils/config');
+
+      module.config();
+
+      expect(dotenv.config).toBeCalled();
+      expect(path.join).not.toBeCalled();
+    });
   });
 });
